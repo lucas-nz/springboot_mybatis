c0906f70fcf8655184df95f25d495325a0a5f989
zhousz <13687646803@163.com>
2018-06-07 21:29:01 +0800

    first commit

---
+++ src/main/java/com/zhousz/main/SpringMybatisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zhousz.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisDemoApplication.class, args);
+    }
+}
